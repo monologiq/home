21eb7bed4ef3c056fa806006cc916b7daac25c63
monologiq <git@pmlogue.me>
2025-12-04 09:17:02 +0100

    bin: Create directory and add darkmode bin

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ symlink() {
     stow -v --restow --no-folding --stow "$1" --target "$2"
 }
 
+symlink "bin" "$HOME/.local/bin"
 symlink "git" "$HOME/.config/git"
+symlink "gtk-3.0" "$HOME/.config/gtk-3.0"
 symlink "kitty" "$HOME/.config/kitty"
 symlink "mozilla" "$HOME/.mozilla"
 symlink "zsh" "$HOME/.config/zsh"
