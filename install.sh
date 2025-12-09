01ca5fef035335d5e76b73042ce183105497d5f8
Paul-Mathias Logue <git@pmlogue.me>
2025-12-09 22:03:20 +0100

    fonts: Finish font setup

--- install.sh
+++ install.sh
@@ -10,6 +10,11 @@ symlink() {
 symlink "alacritty" "$HOME/.config/alacritty"
 symlink "bin" "$HOME/.local/bin"
 symlink "fonts" "$HOME/.local/share/fonts"
+symlink "fontconfig" "$HOME/.config/fontconfig"
+ln -sf /usr/share/fontconfig/conf.avail/10-antialias.conf            $XDG_CONFIG_HOME/fontconfig/conf.d/
+ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf       $XDG_CONFIG_HOME/fontconfig/conf.d/
+ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf        $XDG_CONFIG_HOME/fontconfig/conf.d/
+ln -sf /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf    $XDG_CONFIG_HOME/fontconfig/conf.d/
 symlink "git" "$HOME/.config/git"
 symlink "gtk-3.0" "$HOME/.config/gtk-3.0"
 symlink "kitty" "$HOME/.config/kitty"
